a708d1c5ddada503485b87835d737fb04559a286
Copycat_mime <166753392+alen162021@users.noreply.github.com>
2026-04-02 14:52:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
-
-
 import streamlit as st
 import numpy as np
 import librosa
 import joblib
-
-import os
-st.write("Files in directory:", os.listdir())
 
 st.title("Delamination Detection using Acoustic Signals")
 
 # Load trained model
 model = joblib.load("model.pkl")
 
-# Feature extraction (same as your training)
 def extract_features(signal, sr):
     mfcc = librosa.feature.mfcc(y=signal, sr=sr, n_mfcc=13)
     mfcc_mean = np.mean(mfcc, axis=1)
@@ -24,8 +18,7 @@ def extract_features(signal, sr):
 
     return np.hstack([mfcc_mean, psd_mean])
 
-# Upload audio
-uploaded_file = st.file_uploader("Upload a WAV audio file", type=["wav"])
+uploaded_file = st.file_uploader("Upload WAV file", type=["wav"])
 
 if uploaded_file is not None:
     st.audio(uploaded_file)
@@ -36,6 +29,6 @@ if uploaded_file is not None:
     prediction = model.predict([features])
 
     if prediction[0] == 0:
-        st.success("Prediction: GOOD (No Delamination)")
+        st.success("GOOD (No Delamination)")
     else:
-        st.error("Prediction: BAD (Delamination Detected)")
+        st.error("BAD (Delamination Detected)")
